ff6c77dda627fc3195290a9d5221deb2b3335bd5
Jimmy404 <bruno_pier@hotmail.com>
2020-11-25 11:51:09 -0300

    Adds a category list model

--- src/components/GifExpertApp.js
+++ src/components/GifExpertApp.js
@@ -1,10 +1,27 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 export const GifExpertApp = () => {
+
+  // const categories = ;
+  const [categories, setCategories] = useState(['LOTR', 'Hobbit', 'Silmarillion']);
+
+  const handleAdd = () => {
+    setCategories(cats => [...cats, 'Elfs']);
+  }
+
   return (
     <>
       <h2>GifExpertApp</h2>
       <hr />
+
+      <button onClick={handleAdd}>Add</button>
+
+      <ol>
+        {categories.map(category => {
+          return <li key={category}>{ category }</li>
+        })}
+      </ol>
+
     </>
   )
 }
