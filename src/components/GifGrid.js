0944e6d3d13411175fa99b81090668452bf09458
Jimmy404 <bruno_pier@hotmail.com>
2020-11-25 20:58:58 -0300

    Adds helper folder & code

--- src/components/GifGrid.js
+++ src/components/GifGrid.js
@@ -1,28 +1,15 @@
 import React, { useState, useEffect } from 'react';
 import { GifGridItem } from '../components/GifGridItem';
+import { getGifs } from '../helpers/getGifs';
 
 export const GifGrid = ({category}) => {
 
   const [images, setImages] = useState([]);
 
   useEffect(() => {
-    getGifs();
-  }, []);
-
-  const getGifs = async() => {
-    const url = 'https://api.giphy.com/v1/gifs/search?q=digimon&limit=10&api_key=UiHvQwmEAGOZk1fLzAzBXKJnRaqgQPRE';
-    const response = await fetch(url);
-    const {data} = await response.json();
-    const gifs = data.map( img => {
-      return{
-        id: img.id,
-        title: img.title,
-        url: img.images?.downsized_medium.url
-      }
-    })
-    console.log(gifs);
-    setImages(gifs);
-  }
+    getGifs(category)
+      .then(setImages);
+  }, [category]);
 
   return (
     <>
